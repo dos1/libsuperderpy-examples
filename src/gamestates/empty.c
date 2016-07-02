cbf79d5425b959e8113e49d946658b83778147af
Sebastian Krzyszkowiak <dos@dosowisko.net>
2016-07-03 00:58:38 +0200

    initial commit with nothing to see there

---
+++ src/gamestates/empty.c
@@ -0,0 +1,82 @@
+/*! \file empty.c
+ *  \brief Empty gamestate.
+ */
+/*
+ * Copyright (c) Sebastian Krzyszkowiak <dos@dosowisko.net>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+#include <libsuperderpy.h>
+#include "empty.h"
+
+int Gamestate_ProgressCount = 1; // number of loading steps as reported by Gamestate_Load
+
+void Gamestate_Logic(struct Game *game, struct EmptyResources* data) {
+	// Called 60 times per second. Here you should do all your game logic.
+	data->blink_counter++;
+	if (data->blink_counter >= 60) {
+		data->blink_counter = 0;
+	}
+}
+
+void Gamestate_Draw(struct Game *game, struct EmptyResources* data) {
+	// Called as soon as possible, but no sooner than next Gamestate_Logic call.
+	// Draw everything to the screen here.
+	if (data->blink_counter < 50) {
+		al_draw_text(data->font, al_map_rgb(255,255,255), game->viewport.width / 2, game->viewport.height / 2, ALLEGRO_ALIGN_CENTRE, "Nothing to see here, move along!");
+	}
+}
+
+void Gamestate_Start(struct Game *game, struct EmptyResources* data) {
+	// Called when this gamestate gets control. Good place for initializing state,
+	// playing music etc.
+	data->blink_counter = 0;
+}
+
+void Gamestate_ProcessEvent(struct Game *game, struct EmptyResources* data, ALLEGRO_EVENT *ev) {
+	// Called for each event in Allegro event queue.
+	// Here you can handle user input, expiring timers etc.
+	if ((ev->type==ALLEGRO_EVENT_KEY_DOWN) && (ev->keyboard.keycode == ALLEGRO_KEY_ESCAPE)) {
+		UnloadGamestate(game, "empty"); // mark this gamestate to be stopped and unloaded
+		// When there are no active gamestates, the engine will quit.
+	}
+}
+
+void* Gamestate_Load(struct Game *game, void (*progress)(struct Game*)) {
+	// Called once, when the gamestate library is being loaded.
+	// Good place for allocating memory, loading bitmaps etc.
+	struct EmptyResources *data = malloc(sizeof(struct EmptyResources));
+	data->font = al_create_builtin_font();
+	progress(game); // report that we progressed with the loading, so the engine can draw a progress bar
+	return data;
+}
+
+void Gamestate_Stop(struct Game *game, struct EmptyResources* data) {
+	// Called when gamestate gets stopped. Stop timers, music etc. here.
+}
+
+void Gamestate_Unload(struct Game *game, struct EmptyResources* data) {
+	// Called when the gamestate library is being unloaded.
+	// Good place for freeing all allocated memory and resources.
+	al_destroy_font(data->font);
+	free(data);
+}
+
+// Ignore those for now.
+// TODO: Check, comment, refine and/or remove:
+void Gamestate_Reload(struct Game *game, struct EmptyResources* data) {}
+void Gamestate_Pause(struct Game *game, struct EmptyResources* data) {}
+void Gamestate_Resume(struct Game *game, struct EmptyResources* data) {}
---
+++ src/main.c
@@ -0,0 +1,5 @@
+#include "libsuperderpy.h"
+
+int main(int argc, char** argv) {
+	return libsuperderpy(argc, argv);
+}
